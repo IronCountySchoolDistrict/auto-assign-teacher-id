ed8b8bd8c61a95840273359e0e8d53054a856554
Basiczombie <issacgable@gmail.com>
2016-05-19 15:31:49 -0600

    fixed query setup

--- src/scripts/auto-assign-teacher-id/js/main.js
+++ src/scripts/auto-assign-teacher-id/js/main.js
@@ -3,13 +3,19 @@
   $(document).ready(function () {
 
     //fetch js https://github.com/github/fetch json example
-    fetch('/ws/schema/query/org.irondistrict.autoid.queries.teacher')
+    fetch('/ws/schema/query/org.irondistrict.autoid.queries.teacher', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
       .then(function(response){
         return response.json()
       }).then(function (json) {
         var teacherIdField = $('[name=teachernumber]')[0];
-        console.log(json.teacherId);
-        teacherIdField.value = json.TEACHERNUMBER;
+        teacherIdField.value = json.record[0].teachernumber;
         teacherIdField.setAttribute('readonly', 'readonly');
       }).catch(function(ex){
         console.log('parsing error', ex)
