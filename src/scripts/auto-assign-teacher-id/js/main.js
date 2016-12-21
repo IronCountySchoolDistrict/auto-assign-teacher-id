cb26369b5afc930fe87d87ba7c6c2a8d4b8d98e3
jonesnc <purkinje90@gmail.com>
2016-12-21 13:36:34 -0700

    Use es2015 module syntax for main.js

--- src/scripts/auto-assign-teacher-id/js/main.js
+++ src/scripts/auto-assign-teacher-id/js/main.js
@@ -1,7 +1,7 @@
-/*global $j*/
-(function($){
-  $(document).ready(function () {
+import $ from 'jquery';
 
+export default function() {
+  $(document).ready(function() {
     //fetch js https://github.com/github/fetch json example
     fetch('/ws/schema/query/org.irondistrict.autoid.queries.teacher', {
       method: 'POST',
@@ -9,18 +9,15 @@
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
-      },
-    })
-      .then(function(response){
-        return response.json()
-      }).then(function (json) {
-        var teacherIdField = $('[name=teachernumber]')[0];
-        teacherIdField.value = json.record[0].teachernumber;
-        teacherIdField.setAttribute('readonly', 'readonly');
-      }).catch(function(ex){
-        console.log('parsing error', ex)
-      })
-
-
+      }
+    }).then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      var teacherIdField = $('[name=teachernumber]')[0];
+      teacherIdField.value = json.record[0].teachernumber;
+      teacherIdField.setAttribute('readonly', 'readonly');
+    }).catch(function(ex) {
+      console.log('parsing error', ex)
+    });
   });
-})($j);
+}
