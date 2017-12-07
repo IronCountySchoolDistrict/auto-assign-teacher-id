42542e2b4c7830de9722d325ef0c5b67a4a1819a
Nathan Jones <nathan.jones@ironmail.org>
2017-12-07 09:57:12 -0700

    Use tlist_sql instead of PowerQuery

--- src/scripts/auto-assign-teacher-id/js/main.js
+++ src/scripts/auto-assign-teacher-id/js/main.js
@@ -2,22 +2,9 @@ import $ from 'jquery';
 
 export default function() {
   $(document).ready(function() {
-    //fetch js https://github.com/github/fetch json example
-    fetch('/ws/schema/query/org.irondistrict.autoid.queries.teacher', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }).then(function(response) {
-      return response.json()
-    }).then(function(json) {
-      var teacherIdField = $('[name=teachernumber]')[0];
-      teacherIdField.value = json.record[0].teachernumber;
-      teacherIdField.setAttribute('readonly', 'readonly');
-    }).catch(function(ex) {
-      console.log('parsing error', ex)
-    });
+    var teacherNumber = $('#ps-data').data('ps');
+    var teacherIdField = $('[name=teachernumber]')[0];
+    teacherIdField.value = teacherNumber.teacherId;
+    teacherIdField.setAttribute('readonly', 'readonly');
   });
 }
