3e5d1e5bcb5d299705316c12223976a97e6ebccb
Issac Gable <issacgable@gmail.com>
2019-06-24 14:36:08 -0600

    Updated for plugin install

--- src/scripts/auto-assign-teacher-id/js/main.js
+++ src/scripts/auto-assign-teacher-id/js/main.js
@@ -1,10 +1,8 @@
 import $ from 'jquery';
 
-export default function() {
-  $(document).ready(function() {
-    var teacherNumber = $('#ps-data').data('ps');
-    var teacherIdField = $('[name=teachernumber]')[0];
-    teacherIdField.value = teacherNumber.teacherId;
-    teacherIdField.setAttribute('readonly', 'readonly');
-  });
-}
+$(() => {
+  var teacherNumber = $('#ps-data').data('ps');
+  var teacherIdField = $('[name=teachernumber]')[0];
+  teacherIdField.value = teacherNumber.teacherId;
+  teacherIdField.setAttribute('readonly', 'readonly');
+});
